a645faabf1bf92638e5b464b9a629dc7854a7a18
Aleph-Design <jan@aleph-design.nl>
2021-07-25 17:45:28 +0200

    Initial starting version compiling TFT_eSPI

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+// SPIFFS and FS dependancy is 'included' in TFT_eSPI library
+//
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
